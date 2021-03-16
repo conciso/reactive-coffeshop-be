727d7c050187d2147f04e5914f4d558297ea5b94
Lazalatin <lazalatin@tutanota.com>
2021-03-16 14:50:00 +0100

    Initially push generated spring project

---
+++ src/main/java/de/conciso/reactivecoffeeshop/ReactiveCoffeeshopApplication.java
@@ -0,0 +1,13 @@
+package de.conciso.reactivecoffeeshop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReactiveCoffeeshopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReactiveCoffeeshopApplication.class, args);
+	}
+
+}
---
+++ src/test/java/de/conciso/reactivecoffeeshop/ReactiveCoffeeshopApplicationTests.java
@@ -0,0 +1,13 @@
+package de.conciso.reactivecoffeeshop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReactiveCoffeeshopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
